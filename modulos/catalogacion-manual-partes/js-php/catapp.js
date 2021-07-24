6835f2c699e6148a36896e30957a28021570f588
rodrigozxd <rodrigo.paredes@tecsup.edu.pe>
2021-07-23 23:47:28 -0500

    Mostrar informacion Completa con Imagenes
    Informacion traida de la base de datos, ademas de mostrar imagenes, solo se trae informacion de aquellos registros que tengan imagen relacionada

--- modulos/catalogacion-manual-partes/js-php/catapp.js
+++ modulos/catalogacion-manual-partes/js-php/catapp.js
@@ -15,27 +15,49 @@ $(document).ready(function() {
                 success: function(resdata) {
                     let equipos = JSON.parse(resdata);
                     let template = '';
-                    let templateL = '<option >Selecciona Lugar...</option>';
-                    let templateMar = '<option >Selecciona Marca...</option>';
-                    let templateC = '<option >Selecciona Categoria...</option>';
-                    let templateMod = '<option >Selecciona Codigo...</option>';
+                    let templateL = '';
+                    let templateMar = '';
+                    let templateC = '';
+                    let templateMod = '';
+                    let temporal = '';
                     
                     equipos.forEach(equipo => {
-                        template += `
-                        <option value="${equipo.modelo}" />
-                        `;
-                        templateL += `
-                        <option value="${equipo.lugar}">${equipo.lugar}</option>
-                        `;
-                        templateMar += `
-                        <option value="${equipo.marca}">${equipo.marca}</option>
-                        `;
-                        templateC += `
-                        <option value="${equipo.cliente}">${equipo.cliente}</option>
-                        `;
-                        templateMod += `
-                        <option value="${equipo.nombre}">${equipo.nombre}</option>
-                        `;
+
+                        temporal = equipo.modelo;
+                        if (template.indexOf(temporal) == -1 ){
+                            template += `
+                            <option value="${equipo.modelo}" />
+                            `;
+                        }
+
+                        temporal = equipo.lugar;
+                        if (templateL.indexOf(temporal) == -1 ){
+                            templateL += `
+                            <option value="${equipo.lugar}">${equipo.lugar}</option>
+                            `;
+                        }
+
+                        temporal = equipo.marca;
+                        if (templateMar.indexOf(temporal) == -1 ){
+                            templateMar += `
+                            <option value="${equipo.marca}">${equipo.marca}</option>
+                            `;
+                        }
+
+                        temporal = equipo.cliente;
+                        if (templateC.indexOf(temporal) == -1 ){
+                            templateC += `
+                            <option value="${equipo.cliente}">${equipo.cliente}</option>
+                            `;
+                        }
+                        
+                        temporal = equipo.nombre;
+                        if (templateMod.indexOf(temporal) == -1 ){
+                            templateMod += `
+                            <option value="${equipo.nombre}">${equipo.nombre}</option>
+                            `;
+                        }
+
                     });
                     $('#languageList').html(template);
                     $('#selectLugar').html(templateL);
@@ -44,15 +66,7 @@ $(document).ready(function() {
                     $('#selectCodigo').html(templateMod);
                 }
             })
-
-            $.ajax({
-                url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/informacion.php',
-                type: 'POST',
-                data: { search },
-                success: function(resdata) {
-                    
-                }
-            })
+            buscaDetalles(search);
             link = false
         }
     }
@@ -69,27 +83,47 @@ $(document).ready(function() {
                 success: function(resdata) {
                     let equipos = JSON.parse(resdata);
                     let template = '';
-                    let templateL = '<option >Selecciona Lugar...</option>';
-                    let templateMar = '<option >Selecciona Marca...</option>';
-                    let templateC = '<option >Selecciona Categoria...</option>';
-                    let templateMod = '<option >Selecciona Codigo...</option>';
+                    let templateL = '';
+                    let templateMar = '';
+                    let templateC = '';
+                    let templateMod = '';
+                    let temporal = '';
                     
                     equipos.forEach(equipo => {
-                        template += `
-                        <option value="${equipo.modelo}" />
-                        `;
-                        templateL += `
-                        <option value="${equipo.lugar}">${equipo.lugar}</option>
-                        `;
-                        templateMar += `
-                        <option value="${equipo.marca}">${equipo.marca}</option>
-                        `;
-                        templateC += `
-                        <option value="${equipo.cliente}">${equipo.cliente}</option>
-                        `;
-                        templateMod += `
-                        <option value="${equipo.nombre}">${equipo.nombre}</option>
-                        `;
+                        temporal = equipo.modelo;
+                        if (template.indexOf(temporal) == -1 ){
+                            template += `
+                            <option value="${equipo.modelo}" />
+                            `;
+                        }
+
+                        temporal = equipo.lugar;
+                        if (templateL.indexOf(temporal) == -1 ){
+                            templateL += `
+                            <option value="${equipo.lugar}">${equipo.lugar}</option>
+                            `;
+                        }
+
+                        temporal = equipo.marca;
+                        if (templateMar.indexOf(temporal) == -1 ){
+                            templateMar += `
+                            <option value="${equipo.marca}">${equipo.marca}</option>
+                            `;
+                        }
+
+                        temporal = equipo.cliente;
+                        if (templateC.indexOf(temporal) == -1 ){
+                            templateC += `
+                            <option value="${equipo.cliente}">${equipo.cliente}</option>
+                            `;
+                        }
+                        
+                        temporal = equipo.nombre;
+                        if (templateMod.indexOf(temporal) == -1 ){
+                            templateMod += `
+                            <option value="${equipo.nombre}">${equipo.nombre}</option>
+                            `;
+                        }
                     });
                     $('#languageList').html(template);
                     $('#selectLugar').html(templateL);
@@ -98,6 +132,7 @@ $(document).ready(function() {
                     $('#selectCodigo').html(templateMod);
                 }
             })
+            buscaDetalles(search);
             link = false;
         }
     })
@@ -125,71 +160,195 @@ $(document).ready(function() {
         })
     });
 
-    function fillLugar() {
-        $.ajax({
-            url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/place-list.php',
-            type: 'GET',
-            success: function (response) {
-                let lugares = JSON.parse(response);
-                let template = '<option >Lugar...</option>';
-                lugares.forEach(lugar => {
-                    template += `
-                        <option>${lugar.lugar}</option>
-                    `;
-                });
-                $('#selectLugar').html(template)
-            }
-        });
-    }
-    function fillMarcas() {
-        $.ajax({
-            url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/marca-list.php',
-            type: 'GET',
-            success: function (response) {
-                let marcas = JSON.parse(response);
-                let template = '<option >Marcas...</option>';
-                marcas.forEach(marca => {
-                    template += `
-                        <option>${marca.marca}</option>
-                    `;
-                });
-                $('#selectMarca').html(template)
-            }
-        });
-    }
-
-    function fillClientes() {
-        $.ajax({
-            url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/cliente-list.php',
-            type: 'GET',
-            success: function (response) {
-                let clientes = JSON.parse(response);
-                let template = '<option >Clientes...</option>';
-                clientes.forEach(cliente => {
-                    template += `
-                        <option>${cliente.cliente}</option>
-                    `;
-                });
-                $('#selectCliente').html(template)
-            }
-        });
-    }
-
-    function fillModelos() {
-        $.ajax({
-            url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/models-list.php',
-            type: 'GET',
-            success: function (response) {
-                let models = JSON.parse(response);
-                let template = '<option >Codigos...</option>';
-                models.forEach(mods => {
-                    template += `
-                        <option>${mods.modelo}</option>
-                    `;
-                });
-                $('#selectCodigo').html(template)
-            }
-        });
-    }
-
+    
 });
+
+function buscaDetalles(busqueda){
+    $.ajax({
+        url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/informacion.php',
+        type: 'POST',
+        data: { search:busqueda },
+        success: function(resdata) {
+            let detalles = JSON.parse(resdata);
+            let template = '';
+            let templateImagen = '';
+            let templateDetalles = '';
+            let temporal = '';
+
+            detalles.forEach(detalle => {
+
+                temporal = detalle.archivo;
+                if (template.indexOf(temporal) == -1){
+                    template += `
+                    <tr role="row" class="odd">
+                        <td>${detalle.archivo}</td>
+                        <td>${detalle.tipoArchivo}</td>
+                    </tr>
+                    `;
+                }
+
+                temporal = detalle.archivo;
+                if (templateImagen.indexOf(temporal) == -1){
+                    templateImagen += `
+                    <img src="http://localhost/JPMODULAR/dist/img/equipos/${detalle.archivo}" usemap="#image-map">
+                    `;
+                }
+
+
+                temporal = detalle.nombre;
+                if (templateDetalles.indexOf(temporal) == -1){
+                    templateDetalles += `
+
+                    <div class="row">
+                        <div class="col-sm-6">${detalle.nombre}</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                                <thead>
+                                    <tr role="row">
+                                    <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Motor de renderizado: activar para ordenar la columna descendente">
+                                        <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Informacion</font>
+                                        </font>
+                                    </th>
+                                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Navegador: activar para ordenar la columna de forma ascendente">
+                                        <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Valor</font>
+                                        </font>
+                                    </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr role="row" class="odd">
+                                        <th>Codigo Equipo</th>
+                                        <td>${detalle.nombre}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Estado</th>
+                                        <td>${detalle.estado}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Presion Alta</th>
+                                        <td>${detalle.presion_alta}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Presion Baja</th>
+                                        <td>${detalle.presion_baja}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Frecuencia</th>
+                                        <td>${detalle.frecuencia}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Capacidad</th>
+                                        <td>${detalle.capacidad}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Refrigerante</th>
+                                        <td>${detalle.refrigerante}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Tipo</th>
+                                        <td>${detalle.tipo}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Numero de Serie</th>
+                                        <td>${detalle.numero_serie}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Voltaje</th>
+                                        <td>${detalle.voltaje}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Amperaje</th>
+                                        <td>${detalle.amperaje}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Tipo de Equipo</th>
+                                        <td>${detalle.tipoEquipo}</td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <th>Observacion Calefaccion</th>
+                                        <td>${detalle.observacion}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    `;
+                }
+            })
+            $('#tableArchivos').html(template);
+            $('#example2_wrapper').html(templateDetalles);
+            $('#divImg').html(templateImagen);
+        }
+    })
+}
+
+function fillLugar() {
+    $.ajax({
+        url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/place-list.php',
+        type: 'GET',
+        success: function (response) {
+            let lugares = JSON.parse(response);
+            let template = '<option >Lugar...</option>';
+            lugares.forEach(lugar => {
+                template += `
+                    <option>${lugar.lugar}</option>
+                `;
+            });
+            $('#selectLugar').html(template)
+        }
+    });
+}
+function fillMarcas() {
+    $.ajax({
+        url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/marca-list.php',
+        type: 'GET',
+        success: function (response) {
+            let marcas = JSON.parse(response);
+            let template = '<option >Marcas...</option>';
+            marcas.forEach(marca => {
+                template += `
+                    <option>${marca.marca}</option>
+                `;
+            });
+            $('#selectMarca').html(template)
+        }
+    });
+}
+
+function fillClientes() {
+    $.ajax({
+        url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/cliente-list.php',
+        type: 'GET',
+        success: function (response) {
+            let clientes = JSON.parse(response);
+            let template = '<option >Clientes...</option>';
+            clientes.forEach(cliente => {
+                template += `
+                    <option>${cliente.cliente}</option>
+                `;
+            });
+            $('#selectCliente').html(template)
+        }
+    });
+}
+
+function fillModelos() {
+    $.ajax({
+        url: 'http://localhost/JPMODULAR/modulos/catalogacion-manual-partes/js-php/models-list.php',
+        type: 'GET',
+        success: function (response) {
+            let models = JSON.parse(response);
+            let template = '<option >Codigos...</option>';
+            models.forEach(mods => {
+                template += `
+                    <option>${mods.modelo}</option>
+                `;
+            });
+            $('#selectCodigo').html(template)
+        }
+    });
+}
--- modulos/catalogacion-manual-partes/recursos/jp.php
+++ modulos/catalogacion-manual-partes/recursos/jp.php
@@ -8,43 +8,45 @@
   }
 
   .main-container-flex {
-    border: 1px solid red;
+
     display: flex;
     justify-content: space-between;
   }
 
   .flex-column-div {
-    width: 610px;
+    width: 40%;
   }
 
   .flex-column-div1 {
-    margin-left: 30px;
-    margin-right: 50px;
-    width: 500px;
+    margin-left: 20px;
+    margin-right: 20px;
+    width: 35%;
   }
 </style>
 <div class="main-container">
   <div class="main-container-flex">
-    <div class="flex-column-div" style="border:1px solid blue;">
+    <div class="flex-column-div">
       <p>INFORMACION</p>
 
       <nav class="navbar navbar-light bg-light">
         <form class="form-inline">
-        <?php 
+          <?php
 
-        if (isset($_REQUEST['modelo'])){
-          $modelo = $_REQUEST['modelo'];
-        ?>
-          <input type="text" placeholder="Search" aria-label="Search" class="form-control mr-sm-2" id="search" list="languageList" value=<?=$modelo ?> ><!--your input textbox-->
-        <?php
-        }else{
-        ?>
-          <input type="text" placeholder="Search" aria-label="Search" class="form-control mr-sm-2" id="search" list="languageList" ><!--your input textbox-->
-        <?php
-        }
-        ?>
-            <datalist id="languageList">
-            </datalist>
+          if (isset($_REQUEST['modelo'])) {
+            $modelo = $_REQUEST['modelo'];
+          ?>
+            <input type="text" placeholder="Search" aria-label="Search" class="form-control mr-sm-2" id="search" list="languageList" value=<?= $modelo ?>>
+            <!--your input textbox-->
+          <?php
+          } else {
+          ?>
+            <input type="text" placeholder="Search" aria-label="Search" class="form-control mr-sm-2" id="search" list="languageList">
+            <!--your input textbox-->
+          <?php
+          }
+          ?>
+          <datalist id="languageList">
+          </datalist>
 
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
         </form>
@@ -63,14 +65,14 @@
       <div class="form-group col-md-6">
         <label for="selectMarca">Marca:</label>
         <select type="text" id="selectMarca" class="form-control">
-          
+
         </select>
       </div>
 
       <div class="form-group col-md-6">
         <label for="selectCliente">Cliente:</label>
         <select id="selectCliente" class="form-control">
-          
+
         </select>
       </div>
 
@@ -80,24 +82,73 @@
           <!-- LLENADO POR PHP/AJAX -->
         </select>
       </div>
+
       <br>
-      <div class="flex-column-div1" >
-        <ul id="detalles"></ul>
+
+      <!-- LISTADO DE ARCHIVOS EN TABLA -->
+      <h6>Archivos de Equipo</h6>
+      <div class="flex-column-div1">
+        <div class="box-body">
+          <div id="example1_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            <div class="row">
+              <div class="col-sm-6"></div>
+              <div class="col-sm-6"></div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12">
+                <table id="example2" class="table table-bordered table-hover dataTable" role="grid" aria-describedby="example2_info">
+                  <thead>
+                    <tr role="row">
+                      <th class="sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Motor de renderizado: activar para ordenar la columna descendente">
+                        <font style="vertical-align: inherit;">
+                          <font style="vertical-align: inherit;">Nombre</font>
+                        </font>
+                      </th>
+                      <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Navegador: activar para ordenar la columna de forma ascendente">
+                        <font style="vertical-align: inherit;">
+                          <font style="vertical-align: inherit;">Tipo</font>
+                        </font>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody id="tableArchivos">
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
+      <h6>Detalles de Equipo</h6>
+      <div class="flex-column-div1">
+        <div class="box-body">
+          <div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
+            
+          </div>
+        </div>
+      </div>
+
     </div>
-    <div class="flex-column-div" >
+
+
+
+
+    <!-- Mostrado de Archivos Dinamicos -->
+    <div id="divImg" >
 
       <?php
-      $interacciones = '<img src="https://www.sdlgla.com/wp-content/uploads/2018/03/img_paginaproduto_01.jpg" usemap="#image-map">
+      /* $interacciones = '<img src="https://www.sdlgla.com/wp-content/uploads/2018/03/img_paginaproduto_01.jpg" usemap="#image-map">
                           <map name="image-map">
                               <area target="" alt="Cabina" title="Cabina" href="http://localhost/JPMODULAR/main/lista-equipo/797f" coords="331,168,328,188,334,197,334,224,318,260,314,293,308,319,322,308,359,305,391,305,421,313,442,335,461,358,476,370,512,368,514,317,513,285,534,279,547,267,526,217,519,187,486,168" shape="poly">
                           </map>';
-      echo $interacciones;
+      echo $interacciones; */
       ?>
 
     </div>
   </div>
 </div>
 
-<script src="<?=$direccion?>js/vendor/jquery-1.11.3.min.js"></script>
-<script src="<?=$direccion?>modulos/catalogacion-manual-partes/js-php/catapp.js"></script>
+<script src="<?= $direccion ?>js/vendor/jquery-1.11.3.min.js"></script>
+<script src="<?= $direccion ?>modulos/catalogacion-manual-partes/js-php/catapp.js"></script>
